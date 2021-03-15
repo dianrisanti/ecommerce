d7cd48cb86a59e612ed01e1bdc6e6212ca00bdf2
LiemCuSun <cusunliem@gmail.com>
2021-03-15 12:00:17 +0700

    initial commit

---
+++ Final_Project_Group_3/Back_End/database/index.js
@@ -0,0 +1,13 @@
+// NOTE setup mysql
+let mysql = require('mysql') // NOTE untuk menyambungkan api dengan mysql database
+
+
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+module.exports = connection
---
+++ Final_Project_Group_3/Back_End/index.js
@@ -0,0 +1,34 @@
+// import module
+let express = require('express') // NOTE pembuat server pengganti http module
+let bodyParser = require('body-parser') // NOTE untuk menampung req.body
+let cors = require('cors') // NOTE izin sharing data / authorization 
+require('dotenv').config()
+
+// NOTE create app
+let app = express()
+
+// NOTE import Connection MySql
+const connection = require('./database')
+// NOTE untuk check database
+// console.log(connection)
+
+// NOTE apply middleware
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+// NOTE connect database
+connection.connect((err) => {
+    if (err) return console.log(`Oops something wrong ! error code: ${err.stack}`)
+    console.log(`connected as id : ${connection.threadId}`)
+})
+
+// NOTE create home route
+app.get('/', (req, res) => {
+    res.status(200).send(`<h1>This is Home</h1>`)
+})
+
+
+let PORT = 2000
+app.listen(PORT, () => console.log(`Connected to PORT: ${PORT}`))
