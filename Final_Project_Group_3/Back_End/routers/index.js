71295523c6c9ad38ecab61fcdf4fc105a52f5158
dianrisanti <diannnyyy@gmail.com>
2021-03-18 14:58:07 +0700

    fixing

--- Final_Project_Group_3/Back_End/routers/index.js
+++ Final_Project_Group_3/Back_End/routers/index.js
@@ -1,9 +1,11 @@
 let userRouter = require('./userRouter')
 // let profileRouter = require('./profileRouter')
 const productRouter = require('./productRouter')
+const orderRouter = require('./orderRouter')
 
 module.exports = {
     userRouter,
     // profileRouter,
-    productRouter
+    productRouter,
+    orderRouter
 }
---
+++ Final_Project_Group_3/Back_End/routers/orderRouter.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+
+const orderController = require('../controllers/orderController')
+
+router.post('/add', orderController.addCart)
+router.get('/get/userid=:id', orderController.getCart)
+router.post('/edit', orderController.editCart)
+router.post('/delete', orderController.deleteCart)
+
+module.exports = router
