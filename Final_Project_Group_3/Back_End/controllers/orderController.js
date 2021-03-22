95caab1a3d1dd6b70ee72462fb5a965a27504999
dianrisanti <diannnyyy@gmail.com>
2021-03-22 10:31:54 +0700

    fixing place order

--- Final_Project_Group_3/Back_End/controllers/orderController.js
+++ Final_Project_Group_3/Back_End/controllers/orderController.js
@@ -117,7 +117,8 @@ module.exports = {
     getHistory: async(req, res) => {
         try{
             const id_user = req.params.id ? +req.params.id  : 0
-            const history = `SELECT o.date , o.order_number, od.id_product, p.name, od.quantity, p.price, od.total,  pi.image, os.status
+            const history = `SELECT o.date , o.order_number, od.id_product, p.name, od.quantity, 
+            p.price, od.total,  pi.image, os.status, o.payment_method
             FROM orders o
             JOIN order_details od ON o.order_number = od.order_number
             JOIN products p ON od.id_product = p.id
@@ -132,6 +133,7 @@ module.exports = {
                 let temp = {
                     order_number: item.order_number,
                     date: item.date,
+                    payment_method: item.payment_method,
                     status: item.status,
                     products: [{
                         id_product: item.id_product,
@@ -184,7 +186,10 @@ module.exports = {
     },
     getInvoice: async(req, res) => {
         try{
-            const id_user = req.params.id ? +req.params.id  : 0
+            const id_user = +req.params.id
+            console.log('id user ', id_user)
+            const { order_number, payment_method } = req.body
+
             const query = `SELECT o.order_number, o.payment_method, sum(od.total) total, p.address, x.username
             FROM orders o
             JOIN order_details od ON o.order_number = od.order_number
@@ -193,8 +198,10 @@ module.exports = {
             WHERE o.status = 1 AND o.id_user = ${id_user}
             GROUP BY od.order_number`
             const summary = await asyncQuery(query)
+            console.log(summary)
+
             const date = new Date().toDateString()
-            summary[0].date = date
+            
             const currencyFractionDigits = new Intl.NumberFormat('de-DE', {
                 style: 'currency',
                 currency: 'IDR',
@@ -203,7 +210,7 @@ module.exports = {
             var value = summary[0].total.toLocaleString('de-DE', { maximumFractionDigits: currencyFractionDigits });
             
             console.log(value);
-            summary[0].total_IDR = ("IDR " + value)
+            const total_IDR = ("IDR " + value)
 
 
             console.log(summary[0])
@@ -227,13 +234,17 @@ module.exports = {
             option.html = template({ 
                 name: summary[0].username, 
                 order_number: summary[0].order_number, 
-                date: summary[0].date, 
-                payment_method: summary[0].payment_method,
-                total: summary[0].total_IDR
+                date: date, 
+                payment_method: payment_method,
+                total: total_IDR
             })
 
             // send email
             const info = await transporter.sendMail(option)
+
+            const update = `UPDATE orders SET payment_method = '${payment_method}', status = 2 
+            WHERE order_number = '${order_number}' AND id_user = ${id_user}`
+            await asyncQuery(update)
 
             res.status(200).send(info.response)
         }
